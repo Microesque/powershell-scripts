4ad5d3200b707b101287c93bf816c2426220a499
Microesque <microesque@gmail.com>
2025-12-25 10:34:59 +0300

    feat(WSLSetup): add self elevation

---
+++ scripts/WSLSetup.ps1
@@ -0,0 +1,9 @@
+# ==============================================================================
+# =============================== Self Elevation ===============================
+# ==============================================================================
+$CurrentPrincipal = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$Admin = [Security.Principal.WindowsBuiltinRole]::Administrator
+if (-not $CurrentPrincipal.IsInRole($Admin)) {
+    Start-Process Powershell.exe -Verb RunAs -ArgumentList "-file `"$($myinvocation.MyCommand.Definition)`""
+    exit
+}
