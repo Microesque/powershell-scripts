6043736e5c38b36270df9344703137ca3022160c
Microesque <microesque@gmail.com>
2026-01-07 19:06:28 +0300

    feat(aceonline): add scalar sql execution function

--- aceonline/modules/SqlUtils.psm1
+++ aceonline/modules/SqlUtils.psm1
@@ -1,6 +1,6 @@
-# Involkes a sql statement as non-query and returns the number of rows affected.
-# Throws if something goes wrong
-# Returns the number of rows affected
+# Invokes an sql statement as non-query and returns the number of rows affected.
+# Throws if something goes wrong.
+# Returns the number of rows affected.
 function Invoke-SqlExecuteNonQuery {
     param (
         [Parameter(Mandatory)]
@@ -35,4 +35,41 @@ function Invoke-SqlExecuteNonQuery {
     return $rowsAffected
 }
 
-Export-ModuleMember -Function Invoke-SqlExecuteNonQuery
+# Invokes the given query and returns the first row and first column value.
+# Throws if something goes wrong.
+# Returns a single value.
+function Invoke-SqlExecuteScalar {
+    param (
+        [Parameter(Mandatory)]
+        [string]$ConnectionString,
+
+        [Parameter(Mandatory)]
+        [string]$Query
+    )
+
+    try {
+        $connection = New-Object System.Data.SqlClient.SqlConnection
+        $connection.ConnectionString = $ConnectionString
+        $connection.Open()
+    }
+    catch {
+        $connection.Close()
+        throw "Database connection failed to open:`n$_"
+    }
+
+    try {
+        $command = $connection.CreateCommand()
+        $command.CommandText = $Query
+        $rowsAffected = $command.ExecuteScalar()
+    }
+    catch {
+        throw "Failed executing the query:`n$_"
+    }
+    finally {
+        $connection.Close()
+    }
+
+    return $rowsAffected
+}
+
+Export-ModuleMember -Function Invoke-SqlExecuteNonQuery, Invoke-SqlExecuteScalar
