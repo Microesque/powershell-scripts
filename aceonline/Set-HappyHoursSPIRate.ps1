7d03d90520a1f95e5e95926193bf5d4f2c6ce5ee
Microesque <microesque@gmail.com>
2026-01-11 07:02:13 +0300

    feat(aceonline): implement set happy hour spi rate

---
+++ aceonline/Set-HappyHoursSPIRate.ps1
@@ -0,0 +1,46 @@
+# ==============================================================================
+# =================================== IMPORTS ==================================
+# ==============================================================================
+$ModulesPath = Join-Path $PSScriptRoot "modules"
+Import-Module (Join-Path $ModulesPath "CommonUtils.psm1") -Force
+Import-Module (Join-Path $ModulesPath "SqlExpressUtils.psm1") -Force
+
+# ==============================================================================
+# =================================== SCRIPT ===================================
+# ==============================================================================
+# Get user input
+Clear-Host
+try {
+    $server, $username, $password = Read-ServerAndCredentials
+}
+catch {
+    Stop-ScriptWithErrorMessage $_.Exception.Message
+}
+$value = (Read-Host "Enter the new happy hour spi multiplier [0.0-100.0]").Trim()
+if (-not [double]::TryParse($value, [ref]$value) -or $value -lt 0.0 -or $value -gt 100.0) {
+    Stop-ScriptWithErrorMessage "Invalid multiplier value. Multiplier has to be a number between [0.0 - 100.0]."
+}
+$table = "atum2_db_account.dbo.ti_HappyHourEvent"
+$whereCondition = "UniqueNumber > 100"
+
+# Set new value
+try {
+    $columnsAffected = Set-TableColumnValues `
+        -Server $server `
+        -Username $username `
+        -Password $password `
+        -Table $table `
+        -Column "SPIRate" `
+        -Value "$value" `
+        -WhereCondition $whereCondition
+    if ($columnsAffected -eq 0) {
+        throw "Number of rows affected was 0!"
+    }
+}
+catch {
+    Stop-ScriptWithErrorMessage "Something went wrong during SQL execution:`n$($_.Exception.Message)"
+}
+
+# Update user and exit
+Write-Host "Happy hours spi rate is set to $($value * 100)%. (Requires server restart!)" -ForegroundColor Green
+Stop-ScriptWithSuccessMessage "Script successful."
