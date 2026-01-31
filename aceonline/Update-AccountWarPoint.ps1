fcd760c589c7d53c0a7b7a93abf379478a8cc8c1
Microesque <microesque@gmail.com>
2026-01-31 18:45:36 +0300

    feat(ace): implement non-interactive calling

--- aceonline/Update-AccountWarPoint.ps1
+++ aceonline/Update-AccountWarPoint.ps1
@@ -1,4 +1,26 @@
 # ==============================================================================
+# =================================== PARAMS ===================================
+# ==============================================================================
+[CmdletBinding()]
+param (
+    [string]$Server,
+    [string]$Username,
+    [string]$Password,
+    [string]$AccountName,
+    [string]$Value,
+
+    [switch]$NonInteractive
+)
+
+if ($NonInteractive) {
+    foreach ($name in "Server", "Username", "Password", "AccountName", "Value") {
+        if (-not $PSBoundParameters.ContainsKey($name)) {
+            Stop-ScriptWithErrorMessage "Non-interactive execution missing $name parameter."
+        }
+    }
+}
+
+# ==============================================================================
 # =================================== IMPORTS ==================================
 # ==============================================================================
 $ModulesPath = Join-Path $PSScriptRoot "modules"
@@ -6,59 +28,90 @@ Import-Module (Join-Path $ModulesPath "CommonUtils.psm1") -Force
 Import-Module (Join-Path $ModulesPath "SqlExpressUtils.psm1") -Force
 
 # ==============================================================================
+# =========================== PARAM/INPUT VALIDATION ===========================
+# ==============================================================================
+if ($NonInteractive) {
+    $Server = $Server.Trim()
+    $Username = $Username.Trim()
+    $Password = $Password.Trim()
+    $AccountName = $AccountName.Trim()
+    $Value = $Value.Trim()
+
+    try {
+        Assert-ServerAndCredentials $Server $Username $Password
+    }
+    catch {
+        Stop-ScriptWithErrorMessage $_.Exception.Message
+    }
+
+    if ([string]::IsNullOrEmpty($AccountName)) {
+        Stop-ScriptWithErrorMessage "Account name empty!"
+    }
+
+    if ([string]::IsNullOrEmpty($Value)) {
+        Stop-ScriptWithErrorMessage "Value empty!"
+    }
+    elseif ($Value -notmatch "^[+-]?\d+$") {
+        Stop-ScriptWithErrorMessage "Value needs to be an integer, optionally prefixed with + or - -> $Value"
+    }
+}
+else {
+    try {
+        $Server, $Username, $Password = Read-ServerAndCredentials
+    }
+    catch {
+        Stop-ScriptWithErrorMessage $_.Exception.Message
+    }
+
+    $AccountName = (Read-Host "Enter game account name").Trim()
+    if ([string]::IsNullOrEmpty($AccountName)) {
+        Stop-ScriptWithErrorMessage "Account name empty!"
+    }
+
+    $Value = (Read-Host "War points value to set (prefix with + or - to add or subtract instead)").Trim()
+    if ([string]::IsNullOrEmpty($Value)) {
+        Stop-ScriptWithErrorMessage "Value empty!"
+    }
+    elseif ($Value -notmatch "^[+-]?\d+$") {
+        Stop-ScriptWithErrorMessage "Value needs to be an integer, optionally prefixed with + or - -> $Value"
+    }
+}
+
+# ==============================================================================
 # =================================== SCRIPT ===================================
 # ==============================================================================
-# Get user input
-Clear-Host
-try {
-    $server, $username, $password = Read-ServerAndCredentials
-}
-catch {
-    Stop-ScriptWithErrorMessage $_.Exception.Message
-}
-$accountName = (Read-Host "Enter game account name").Trim()
-if ([string]::IsNullOrEmpty($accountName)) {
-    Stop-ScriptWithErrorMessage "Account name empty!"
-}
-$value = (Read-Host "War points value to set (prefix with + or - to add or subtract instead)").Trim()
-if ([string]::IsNullOrEmpty($value)) {
-    Stop-ScriptWithErrorMessage "Value empty!"
-}
-elseif ($value -notmatch "^[+-]?\d+$") {
-    Stop-ScriptWithErrorMessage "Value needs to be an integer, optionally prefixed with + or - -> $value"
-}
 $table = "atum2_db_account.dbo.td_Account"
-$whereCondition = "AccountName = '$accountName'"
+$whereCondition = "AccountName = '$AccountName'"
 
 try {
     # Get the current value
     $currentWarPoint = Get-TableColumnValue `
-        -Server $server `
-        -Username $username `
-        -Password $password `
+        -Server $Server `
+        -Username $Username `
+        -Password $Password `
         -Table $table `
         -Column "WarPoint" `
         -WhereCondition $whereCondition
     if ($currentWarPoint -eq $null) {
-        Stop-ScriptWithErrorMessage "Account name was not found! -> $accountName"
+        Stop-ScriptWithErrorMessage "Account name was not found! -> $AccountName"
     }
 
     # Calculate the value to set
-    if ($value.StartsWith('+') -or $value.StartsWith('-')) {
-        $valueInt = [int]$currentWarPoint + [int]$value
+    if ($Value.StartsWith('+') -or $Value.StartsWith('-')) {
+        $valueInt = [int]$currentWarPoint + [int]$Value
         if ($valueInt -lt 0) {
             $valueInt = 0
         }
     }
     else {
-        $valueInt = [int]$value
+        $valueInt = [int]$Value
     }
     
     #Set the new value
     $columnsAffected = Set-TableColumnValues `
-        -Server $server `
-        -Username $username `
-        -Password $password `
+        -Server $Server `
+        -Username $Username `
+        -Password $Password `
         -Table $table `
         -Column "WarPoint" `
         -Value "$valueInt" `
@@ -68,7 +121,6 @@ catch {
     Stop-ScriptWithErrorMessage "Something went wrong during SQL execution:`n$($_.Exception.Message)"
 }
 
-# Update user and exit
-Write-Host "Account name `"$accountName`" war points was: $currentWarPoint" -ForegroundColor Green
-Write-Host "Account name `"$accountName`" war points set to: $valueInt" -ForegroundColor Green
+Write-Host "Account name `"$AccountName`" war points was: $currentWarPoint" -ForegroundColor Green
+Write-Host "Account name `"$AccountName`" war points set to: $valueInt" -ForegroundColor Green
 Stop-ScriptWithSuccessMessage "Script successful."
