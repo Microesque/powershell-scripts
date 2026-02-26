45d5981a80e7edcb0847e8c8f048146b023006a2
Microesque <microesque@gmail.com>
2026-02-26 10:22:53 +0300

    feat(wsl): implement Debian install/init script

---
+++ wsl/Initialize-Wsl2Debian.ps1
@@ -0,0 +1,107 @@
+# ==============================================================================
+# =================================== IMPORTS ==================================
+# ==============================================================================
+$ModulesPath = Join-Path $PSScriptRoot "modules"
+Import-Module (Join-Path $ModulesPath "CommonUtils.psm1") -Force -ErrorAction Stop
+Import-Module (Join-Path $ModulesPath "WslUtils.psm1") -Force -ErrorAction Stop
+
+# ==============================================================================
+# ================================= ADMIN CHECK ================================
+# ==============================================================================
+if (-not (Test-IsAdministrator)) {
+    throw "This script must be run with administrative privileges."
+}
+
+# ==============================================================================
+# =================================== SCRIPT ===================================
+# ==============================================================================
+$distro = "Debian"
+$defaultUser = "asd"
+$defaultPassword = "asd"
+
+Write-StepTitle "Initializing WSL2 '$distro'"
+
+# Check for an existing installation
+try {
+    if (Test-WSLDistroInstallation $distro) {
+        Write-Log "Distribution [$distro] is already installed in WSL." -Fail
+        exit 1
+    }
+}
+catch {
+    Write-Log "Failed to retrieve the list of installed WSL distributions.`nReason:`n$_" -Fail
+    exit 1
+}
+
+# Update WSL default version to WSL2
+try {
+    $version = Get-WslDefaultVersion
+    if ($version -eq 2) {
+        Write-Log "WSL default version is already set to WSL2." -Success
+    }
+    else {
+        Write-Log "Current default WSL version: WSL$version" -Info
+        Update-Wsl2AsDefault
+        if (Get-WslDefaultVersion -eq 2) {
+            Write-Log "WSL default version is now set to WSL2." -Success
+        }
+        else {
+            Write-Log "WSL default version was set to WSL2, but changes did not take effect." -Fail
+            exit 1
+        }
+    }
+}
+catch {
+    Write-Log "Failed to set the default WSL version to WSL2.`nReason:`n$_" -Fail
+    exit 1
+}
+
+# Install the distro
+try {
+    Write-Log "Installing WSL distribution [$distro]..." -Info
+    Install-WSLDistro $distro
+    Write-Log "WSL distribution [$distro] installed successfully." -Success
+}
+catch {
+    Write-Log "Failed to install WSL distribution [$distro].`nReason:`n$_" -Fail
+    exit 1
+}
+
+Write-StepTitle "Post-install configuration for '$distro'"
+
+# Create a default user
+try {
+    Write-Log "Setting up the default user..." -Info
+    Invoke-WSLRootCommand `
+        -Distro $distro `
+        -Command "adduser --disabled-password --gecos '' $defaultUser; echo '${defaultUser}:${defaultPassword}' | chpasswd; usermod -aG sudo $defaultUser"
+    Write-Log "User [$defaultUser] with the password [$defaultPassword] created." -Success
+
+    Invoke-WSLRootCommand `
+        -Distro $distro `
+        -Command "printf '[user]\ndefault=$defaultUser\n' > /etc/wsl.conf"
+    Write-Log "User [$defaultUser] is now set as the default user for the distribution [$distro]." -Success
+}
+catch {
+    Write-Log "Failed to create default user.`nReason:`n$_" -Fail
+    exit 1
+}
+
+# Initialize distro packages
+try {
+    Write-Log "Updating and upgrading [$distro] packages..." -Info
+    Invoke-WSLRootCommand `
+        -Distro $distro `
+        -Command "set -e; apt update; apt upgrade -y"
+
+    Write-Log "Cleaning up packages..." -Info
+    Invoke-WSLRootCommand `
+        -Distro $distro `
+        -Command "set -e; apt autoremove -y; apt clean"
+
+    Write-Log "Initializing [$distro] packages successful." -Success
+}
+catch {
+    Write-Log "Failed to initialize [$distro] packages.`nReason:`n$_" -Fail
+    exit 1
+}
